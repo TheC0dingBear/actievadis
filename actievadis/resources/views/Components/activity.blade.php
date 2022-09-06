adec9e11d5d7c588f38ca31ca25e9c5abade3028
jasmijn <jasmijnps4@gmail.com>
2022-09-06 18:49:38 +0200

    added activity cards with dummy text

---
+++ actievadis/resources/views/Components/activity.blade.php
@@ -0,0 +1,19 @@
+<div class="activity">
+    <div class="grid grid-cols-3 col-span-1">
+       <div>
+           <p>Activiteit</p>
+       </div>
+
+        <div>
+            <p>Tijd</p>
+        </div>
+
+        <div>
+            <p>Datum</p>
+        </div>
+    </div>
+
+    <div class="h-full flex items-center justify-end">
+       <a href="#">Meer informatie &#8594</a>
+    </div>
+</div>
