2b544c746e1aec95e5706d6187008f629b4f0eae
DylanJanssen1 <dylan.janssen1@student.graafschapcollege.nl>
2022-09-07 13:16:38 +0200

    Changed some data

--- actievadis/database/migrations/2014_10_12_000000_create_users_table.php
+++ actievadis/database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,6 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->boolean('admin');
-            $table->json('singedUpActivities');
             $table->rememberToken();
             $table->timestamps();
         });
