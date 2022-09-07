edd284bb53568ce83e85a736c053b5cc2bceff45
TheC0dingBear <cdusseldorp@live.nl>
2022-09-07 14:48:01 +0200

    commit

---
+++ actievadis/resources/js/Components/Activity.vue
@@ -0,0 +1,62 @@
+<template>
+
+<div class="overflow-x-auto relative">
+    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="py-3 px-6">
+                    Activiteiten
+                </th>
+                <th scope="col" class="py-3 px-6"></th>
+                <th scope="col" class="py-3 px-6"></th>
+                <th scope="col" class="py-3 px-6"></th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Activiteit
+                </th>
+                <td class="py-4 px-6">
+                    datum
+                </td>
+                <td class="py-4 px-6">
+                    tijd
+                </td>
+                <td class="py-4 px-6">
+                    Meer informatie ->
+                </td>
+            </tr>
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                 <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Activiteit
+                </th>
+                <td class="py-4 px-6">
+                    datum
+                </td>
+                <td class="py-4 px-6">
+                    tijd
+                </td>
+                <td class="py-4 px-6">
+                    Meer informatie ->
+                </td>
+            </tr>
+            <tr class="bg-white dark:bg-gray-800">
+                 <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Activiteit
+                </th>
+                <td class="py-4 px-6">
+                    datum
+                </td>
+                <td class="py-4 px-6">
+                    tijd
+                </td>
+                <td class="py-4 px-6">
+                    Meer informatie ->
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
+</template>
--- actievadis/resources/js/app.js
+++ actievadis/resources/js/app.js
@@ -2,6 +2,8 @@ import './bootstrap';
 import { createApp } from 'vue';
 import Header from './components/Header.vue'
 import Footer from './components/Footer.vue'
+import Activity from './Components/Activity.vue'
 
 createApp(Header).mount("#header")
 createApp(Footer).mount("#footer")
+createApp(Activity).mount("#activity")
