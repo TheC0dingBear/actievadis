dd890d67c787bab0aa6aca1fa1722830ab1909e2
jasmijn <jasmijnps4@gmail.com>
2022-09-28 14:32:35 +0200

    homepage is now dynamic

--- actievadis/resources/js/Components/Activity.vue
+++ actievadis/resources/js/Components/Activity.vue
@@ -13,46 +13,18 @@
             </tr>
         </thead>
         <tbody>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Activiteit
+            <tr v-for="activity in activities" :key="activity.id" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th data-toggle="tooltip" v-bind:title="activity.name.length >= 30 ? activity.name : ''" scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    {{ activity.name.length >= 30 ? activity.name.substring(0, 30) + "..." : activity.name }}
                 </th>
-                <td class="py-8 px-8">
-                    datum
+                <td data-toggle="tooltip" v-bind:title="activity.location.length >= 30 ? activity.location : ''" class="py-8 px-8">
+                    {{ activity.location.length >= 30 ? activity.location.substring(0, 30) + "..." : activity.location }}
                 </td>
-                <td class="py-8 px-8">
-                    tijd
+                <td data-toggle="tooltip" class="py-8 px-8">
+                    {{ activity.datum }}
                 </td>
                 <td class="flex justify-center pt-8">
-                    <a href="#" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700"> Meer informatie -></a>
-                </td>
-            </tr>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Activiteit
-                </th>
-                <td class="py-8 px-8">
-                    datum
-                </td>
-                <td class="py-8 px-8">
-                    tijd
-                </td>
-                <td class="flex justify-center pt-8">
-                    <a href="#" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700"> Meer informatie -></a>
-                </td>
-            </tr>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Activiteit
-                </th>
-                <td class="py-8 px-8">
-                    datum
-                </td>
-                <td class="py-8 px-8">
-                    tijd
-                </td>
-                <td class="flex justify-center pt-8">
-                    <a href="#" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700"> Meer informatie -></a>
+                    <a :href="activity.id" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700"> Meer informatie -></a>
                 </td>
             </tr>
         </tbody>
@@ -60,3 +32,21 @@
 </div>
 
 </template>
+
+<script>
+import axios from 'axios';
+export default {
+    data() {
+        return {
+            activities: []
+        }
+    },
+    created() {
+        axios
+            .get('/api/activities/')
+            .then(response => {
+                this.activities = response.data;
+            });
+    }
+}
+</script>
