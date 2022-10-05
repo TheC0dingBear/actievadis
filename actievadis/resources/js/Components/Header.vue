8e7075cfc987cb81f75720d6c0512ce732233871
DylanJanssen1 <dylan.janssen1@student.graafschapcollege.nl>
2022-10-05 12:47:25 +0200

    Progres

--- actievadis/resources/js/Components/Header.vue
+++ actievadis/resources/js/Components/Header.vue
@@ -3,7 +3,7 @@
     <header>
         <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
             <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-                <a href="#" class="flex items-center">
+                <a href="/" class="flex items-center">
                     <img src="../../assets/notext.png" class="w-12" alt="Actievadis Logo"/>
                     <span
                         class="self-center text-xl font-semibold whitespace-nowrap dark:text-gray-300 ml-2">Actievadis
