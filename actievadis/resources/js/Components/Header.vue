5a0075f02d231f73bb2ed8fa7bf74a1334dbcc67
TheC0dingBear <cdusseldorp@live.nl>
2022-10-18 10:57:41 +0200

    actievadis colours

--- actievadis/resources/js/Components/Header.vue
+++ actievadis/resources/js/Components/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <!-- component -->
 <header>
-        <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+        <nav class="bg-white border-b-2 border-[#FDB747] px-4 lg:px-6 py-2.5 dark:bg-gray-800">
             <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                 <a href="/" class="flex items-center">
                     <img src="../../assets/notext.png" class="w-12" alt="Actievadis Logo" />
