191b42bf6514efdc2b475f7901210546cb5d24ec
DylanJanssen1 <dylan.janssen1@student.graafschapcollege.nl>
2022-10-19 13:34:39 +0200

    Remofed the dropdown

--- actievadis/resources/js/Components/Controlpanel/Header.vue
+++ actievadis/resources/js/Components/Controlpanel/Header.vue
@@ -20,48 +20,7 @@
                         </li>
                     </ul>
                 </div>
-                <Menu as="div" class="relative inline-block text-left dark:bg-gray-800">
-                    <menuButton>
-                        <div
-                            class="overflow-hidden relative w-10 h-10 bg-gray-100 rounded-full dark:bg-gray-600">
-                            <svg class="absolute -left-1 w-12 h-12 text-gray-400" fill="currentColor"
-                                 viewBox="0 0 20 20"
-                                 xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-                                      clip-rule="evenodd"></path>
-                            </svg>
-                        </div>
-                    </menuButton>
-
-                    <transition enter-active-class="transition ease-out duration-100"
-                                enter-from-class="transform opacity-0 scale-95"
-                                enter-to-class="transform opacity-100 scale-100"
-                                leave-active-class="transition ease-in duration-75"
-                                leave-from-class="transform opacity-100 scale-100"
-                                leave-to-class="transform opacity-0 scale-95">
-                        <MenuItems
-                            class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                            <div class="py-1">
-                                <MenuItem v-slot="{ active }">
-                                    <a href="/controlpanel/activities/list"
-                                       :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                                           'block px-4 py-2 text-sm']">Activiteiten</a>
-                                </MenuItem>
-                                <MenuItem v-slot="{ active }">
-                                    <a href="/controlpanel/users/list"
-                                       :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                                           'block px-4 py-2 text-sm']">Gebruikers</a>
-                                </MenuItem>
-                            </div>
-                        </MenuItems>
-                    </transition>
-                </Menu>
             </div>
-
         </nav>
     </header>
 </template>
-
-<script setup>
-import {Menu, MenuButton, MenuItem, MenuItems} from '@headlessui/vue'
-</script>
